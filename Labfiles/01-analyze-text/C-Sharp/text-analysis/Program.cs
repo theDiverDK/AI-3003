8ee8b043b1d3d102f0a2f9ba1f98e63e035a604d
Søren Reinke <soren@reinke.dk>
2024-10-25 14:15:06 +0200

    progress

--- Labfiles/01-analyze-text/C-Sharp/text-analysis/Program.cs
+++ Labfiles/01-analyze-text/C-Sharp/text-analysis/Program.cs
@@ -4,7 +4,8 @@ using System.Text;
 using Microsoft.Extensions.Configuration;
 
 // Import namespaces
-
+using Azure;
+using Azure.AI.TextAnalytics;
 
 namespace text_analysis
 {
@@ -20,11 +21,14 @@ namespace text_analysis
                 string aiSvcEndpoint = configuration["AIServicesEndpoint"];
                 string aiSvcKey = configuration["AIServicesKey"];
 
-                // Create client using endpoint and key
 
+                // Create client using endpoint and key
+                AzureKeyCredential credentials = new AzureKeyCredential(aiSvcKey);
+                Uri endpoint = new Uri(aiSvcEndpoint);
+                TextAnalyticsClient aiClient = new TextAnalyticsClient(endpoint, credentials);
 
                 // Analyze each text file in the reviews folder
-                var folderPath = Path.GetFullPath("./reviews");  
+                var folderPath = Path.GetFullPath("./reviews");
                 DirectoryInfo folder = new DirectoryInfo(folderPath);
                 foreach (var file in folder.GetFiles("*.txt"))
                 {
@@ -36,18 +40,49 @@ namespace text_analysis
                     Console.WriteLine("\n" + text);
 
                     // Get language
+                    DetectedLanguage detectedLanguage = aiClient.DetectLanguage(text);
+                    Console.WriteLine($"\nLanguage: {detectedLanguage.Name}");
 
 
                     // Get sentiment
+                    DocumentSentiment sentimentAnalysis = aiClient.AnalyzeSentiment(text);
+                    Console.WriteLine($"\nSentiment: {sentimentAnalysis.Sentiment}");
 
 
                     // Get key phrases
+                    KeyPhraseCollection phrases = aiClient.ExtractKeyPhrases(text);
+                    if (phrases.Count > 0)
+                    {
+                        Console.WriteLine("\nKey Phrases:");
+                        foreach (string phrase in phrases)
+                        {
+                            Console.WriteLine($"\t{phrase}");
+                        }
+                    }
 
 
                     // Get entities
+                    CategorizedEntityCollection entities = aiClient.RecognizeEntities(text);
+                    if (entities.Count > 0)
+                    {
+                        Console.WriteLine("\nEntities:");
+                        foreach (CategorizedEntity entity in entities)
+                        {
+                            Console.WriteLine($"\t{entity.Text} ({entity.Category})");
+                        }
+                    }
 
 
                     // Get linked entities
+                    LinkedEntityCollection linkedEntities = aiClient.RecognizeLinkedEntities(text);
+                    if (linkedEntities.Count > 0)
+                    {
+                        Console.WriteLine("\nLinks:");
+                        foreach (LinkedEntity linkedEntity in linkedEntities)
+                        {
+                            Console.WriteLine($"\t{linkedEntity.Name} ({linkedEntity.Url})");
+                        }
+                    }
 
 
                 }
