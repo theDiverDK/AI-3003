df981b0b1697b51349b0d39ecf3ecc1c76f70b51
Søren Reinke <soren@reinke.dk>
2024-10-28 09:12:11 +0100

    lab 02 done

--- Labfiles/02-qna/C-Sharp/qna-app/Program.cs
+++ Labfiles/02-qna/C-Sharp/qna-app/Program.cs
@@ -3,7 +3,9 @@ using System.IO;
 using System.Text;
 using Microsoft.Extensions.Configuration;
 
-// Import namespaces
+// import namespaces
+using Azure;
+using Azure.AI.Language.QuestionAnswering;
 
 
 namespace qna_app
@@ -23,19 +25,32 @@ namespace qna_app
                 string deploymentName = configuration["QADeploymentName"];
 
                 // Create client using endpoint and key
+                AzureKeyCredential credentials = new AzureKeyCredential(aiSvcKey);
+                Uri endpoint = new Uri(aiSvcEndpoint);
+                QuestionAnsweringClient aiClient = new QuestionAnsweringClient(endpoint, credentials);
 
 
                 // Submit a question and display the answer
-                
-
+                string user_question = "";
+                while (user_question.ToLower() != "quit")
+                {
+                    Console.Write("Question: ");
+                    user_question = Console.ReadLine();
+                    QuestionAnsweringProject project = new QuestionAnsweringProject(projectName, deploymentName);
+                    Response<AnswersResult> response = aiClient.GetAnswers(user_question, project);
+                    foreach (KnowledgeBaseAnswer answer in response.Value.Answers)
+                    {
+                        Console.WriteLine(answer.Answer);
+                        Console.WriteLine($"Confidence: {answer.Confidence:P2}");
+                        Console.WriteLine($"Source: {answer.Source}");
+                        Console.WriteLine();
+                    }
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
         }
-
-
-
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mslearn-ai-language
 Lab files for Azure AI Language modules
+
+Just finished the different Labs
